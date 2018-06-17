71c7b6e1a86481e8ba5d85c49155acd39f3ea9af
Sean DMR <sean@flyinggrizzly.io>
2018-06-17 12:10:11 +0100

    Remove Ubuntu/Oracle files
    No longer needed! Don't work at UoB anymore! Never have to fight with
    Oracle SQL again!

--- ubuntu_xenial_with_oracle_sql_client_libraries/support/oracle_client_rpms/oracle12/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# install rpms and set up global environment field
-
-sudo rpm -i oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm
-sudo rpm -i oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm
-sudo rpm -i oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm
--- ubuntu_xenial_with_oracle_sql_client_libraries/support/scripts/packages.sh
+++
@@ -1,5 +0,0 @@
-apt-get -y install curl 
-apt-get -y install git-core 
-apt-get -y install build-essential 
-apt-get -y install libaio1 
-apt-get -y install alien
